4b57eabd13e24a37fbff1256fa6c6b88544f0636
dpeterson8 <Djp77789@gmail.com>
2023-10-20 20:42:10 -0700

    Got simple enemy ai going

--- Eagle-Jam-Halloween/Assets/Scripts/Enemies/EnemyController.cs
+++ Eagle-Jam-Halloween/Assets/Scripts/Enemies/EnemyController.cs
@@ -1,20 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    bool isResting;
+    public float restTime;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
     int health = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private void Awake() {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange) {
+            Patroling();
+        }
+        if(playerInSightRange && !playerInAttackRange) {
+            ChasePlayer();
+        }
+        if(playerInSightRange && playerInAttackRange) {
+            AttackPlayer();
+        }
+    }
+    private void Patroling() {
+        if(!walkPointSet) {
+            SearchWalkPoint();
+        }
+
+        if(walkPointSet) {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if(distanceToWalkPoint.magnitude < 1f) {
+            if(!isResting) {
+                walkPointSet = false;
+                isResting = true;
+                Invoke(nameof(ResetPatrol), restTime);
+            }
+        }
+    }
+
+    private void SearchWalkPoint() {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer() {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer() {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if(!alreadyAttacked) {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack() {
+        alreadyAttacked = false;
+    }
+
+    private void ResetPatrol() {
+        isResting = false;
     }
 
     public void TakeDamage(int damage) { 
@@ -23,5 +101,14 @@ public class EnemyController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    // Used to draw spehere for attack and sight range
+    // void OnDrawGizmosSelected()
+    // {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireSphere(transform.position, attackRange);
+    //     Gizmos.color = Color.yellow;
+    //     Gizmos.DrawWireSphere(transform.position, sightRange);
+    // }
 
 }
